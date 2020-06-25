3eb91f872dd1e86ee7f0ded24e8726afce923ba9
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-25 21:23:21 +0500

    update task

--- src/dinner.js
+++ src/dinner.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+function Dinner (props) {
+    return (
+        <div>
+        <h1> Today we are Serving {props.dishName}</h1>
+        <h1> Today we are Serving {props.sweetDish}</h1>
+        </div>
+    )
+}
+export default Dinner;
