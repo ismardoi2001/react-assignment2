39149a528f4ee9f1b4fb89a2cf37f04c647bebb8
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-29 22:52:26 +0500

    update task

--- src/dinner.js
+++ src/dinner.js
@@ -4,7 +4,8 @@ function Dinner (props) {
     return (
         <div>
         <h1> Today we are Serving {props.dishName}</h1>
-        <h1> Today we are Serving {props.SweetDish}</h1>
+        <h2> Today we are Serving {props.SweetDish}</h2>
+        <h3> Today we are Serving {props.SweetDish}</h3>
         <hr/>
         </div>
     )
